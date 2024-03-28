e034875b55918e05a85193e4948ae1e69e0b51f0
PHAMTIEN04 <phamphuoctien177@gmail.com>
2024-03-29 00:19:51 +0700

    update

--- BinarySearchTree/KhoitaovaduyetNLR.cpp
+++ BinarySearchTree/KhoitaovaduyetNLR.cpp
@@ -36,17 +36,73 @@ void duyetnlr(Node root){
 		duyetnlr(root->right);
 	}
 }
+void ditimnuthemang(Node &x,Node &y){
+	if(y->left != NULL){
+		ditimnuthemang(x,y->left);
+	}
+	else{
+		x->data = y->data;
+		x = y;
+		y = y->right;
+	}
+}
+void xoa(Node &root,int data){
+	if(root == NULL){
+		return;
+	}
+	else{
+		
+		if(root -> data > data){
+		
+			xoa(root->left,data);
+			
+		}
+		else if(root->data < data){
+		
+			xoa(root->right,data);
+			
+		}
+		else{
+			Node x = root;
+			if(root->left == NULL){
+				root = root->right;
+			}
+			else if(root->right == NULL){
+				root = root->left;
+			}
+			else{
+				Node y = root->right;
+				ditimnuthemang(x,y);
+			}
+			delete x;
+		}
+	}
+}
+Node timkiem(Node root,int data){
+	if(root != NULL){
+		if(root->data > data){
+			timkiem(root->left,data);
+		}
+		else if(root->data < data){
+			timkiem(root->right,data);
+		}
+		else{
+			return root;
+		}
+	}
+}
 int main(){
 	Node t = NULL;
 	int data;
-	for(int i = 0 ; i < 6; i++){
+	for(int i = 0 ; i < 8; i++){
 		cout << "Nhap data: "; cin >> data; cin.ignore();
 		themnut(t,data);
 		cout << endl;
 	}
 	duyetnlr(t);
-
-
+	xoa(t,15);
+	duyetnlr(t);
+	cout << timkiem(t,3)->data;
 	return 0;
 }
 
