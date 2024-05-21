ac4584ed0c0957fd434a68a1dc937ff86263b10d
PHAMTIEN04 <phamphuoctien177@gmail.com>
2024-05-21 23:49:12 +0700

    update

--- Choibai.cpp
+++ Choibai.cpp
@@ -1,31 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main(){
+ 
+
     int n; cin >> n;
+    if(n>= 2){
+	
     int a[n][2];
+    int c1= -1e9,c2 = -1e9;
     for(int i = 0 ; i < n ; i++){
         for(int j = 0; j < 2 ; j++){
             cin >> a[i][j];
+            if(j == 0 && c1 < a[i][j]){
+            	c1 = a[i][j];
+			}
+			if(j == 1 && c2 < a[i][j]){
+				c2 = a[i][j];
+			}
         }
     }
-    int b[2][2];
-    int max = a[0][0];
-    for(int i = 0 ; i < n ; i++){
-        if(if  )
 
-    }
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0; j < 2 ; j++){
-            cout << a[i][j] << " ";
-        }
-        cout << endl;
-    }
-    
-
+	cout << c1+c2 <<endl;
+}
 	return 0;
 }
-// chua xong
-// link : https://coder.husc.edu.vn/problem/hueitc0004 
-
+//chua xong :https://coder.husc.edu.vn/problem/hueitc0004
